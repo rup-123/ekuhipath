77255d9fe224cf95768423632ba90fd3326b772e
Rupjyoti Das <rupjyotidas139@gmail.com>
2023-02-22 22:33:39 +0530

    1st commit

---
+++ app/src/main/java/com/example/e_kuhipath/activities/initials/SplashActivity.kt
@@ -0,0 +1,43 @@
+package com.example.e_kuhipath.activities.initials
+
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.example.e_kuhipath.MainActivity
+import com.example.e_kuhipath.R
+
+class SplashActivity: AppCompatActivity() {
+
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // HERE WE ARE TAKING THE REFERENCE OF OUR IMAGE
+        // SO THAT WE CAN PERFORM ANIMATION USING THAT IMAGE
+        val backgroundImage = findViewById<ImageView>(R.id.SplashScreenImage)
+        val slideAnimation = AnimationUtils.loadAnimation(this, R.anim.top_slide)
+        backgroundImage.startAnimation(slideAnimation)
+
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 3000 is the delayed time in milliseconds.
+    }
+}
